05bbb04b1db4b55250245b04633d1b1ee9fbc5de
Mattia Nicolis <mattia.nicolis04@gmail.com>
2025-11-25 17:52:31 +0100

    creato la cartella database per le credianziale d'accesso a MySQL

---
+++ src/application/database/Database.java
@@ -0,0 +1,18 @@
+package application.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+
+	private static final String URL = "jdbc:mysql://localhost:3306/glicocare";
+	private static final String USER = "root";
+	private static final String PASSWORD = "password";
+
+    private Database() {} //costruttore privato
+    
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
